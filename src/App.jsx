3a0922a36a3a42b3ece3394490ce8391b802906e
jswethaa <jswethaa0604@gmail.com>
2024-12-11 13:04:34 +0530

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Button from "./component/Button"
 import Collaboration from "./component/Collaboration"
 import Header from "./component/Header"
 import Hero from "./component/Hero"
+import Pricing from "./component/Pricing"
+import Roadmap from "./component/Roadmap"
+import Services from "./component/Services"
+import Footer from "./component/Footer"
 function App() {
  
 
@@ -15,6 +19,10 @@ function App() {
         <Hero/>
         <Benefits/>
         <Collaboration/>
+        <Services/>
+        <Pricing/>
+        <Roadmap/>
+        <Footer/>
       </div>
       <ButtonGradient/>
     </>
